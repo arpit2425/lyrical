5b23bd78de81b97ce687e8215073fa131b53d493
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-04 13:32:05 +0530

    version breaking changes fixed

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 class SongList extends Component {
+  renderList() {
+    return this.props.data.songs.map((song) => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    });
+  }
   render() {
-    console.log(this.props);
-    return <div>Songlist</div>;
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+    return <ul className="collection">{this.renderList()}</ul>;
   }
 }
 const query = gql`
   {
     songs {
       title
+      id
     }
   }
 `;
--- client/index.js
+++ client/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-client";
+import { BrowserRouter as Router, Route, hashHistroy } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import SongList from "./components/SongList";
+import App from "./components/App";
 const client = new ApolloClient({});
 const Root = () => {
+  console.log(hashHistroy);
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router histroy={hashHistroy}>
+        <Route path="/" component={App}>
+          <SongList />
+        </Route>
+      </Router>
+      {/* <SongList /> */}
+      {/* <Router>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router> */}
     </ApolloProvider>
   );
 };
